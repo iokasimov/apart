ac0fd0edbcaf47283da9608517c5fc5e40edb460
Murat Kasimov <iokasimov.m@gmail.com>
2018-04-16 23:04:13 +0300

    Add Prefix tree instantiation

--- Data/Tree/Prefix.hs
+++ Data/Tree/Prefix.hs
@@ -1,3 +1,18 @@
-module Data.Tree.Prefix where
+module Data.Tree.Prefix (Prefix) where
+
+import Control.Comonad.Cofree
 
 import Data.Tree (Tree)
+
+type Prefix s a = Tree (Labeled s) a
+
+data Labeled s a = forall t . Traversable t => Hop s (t a)
+
+instance Functor (Labeled s) where
+    fmap f (Hop s as) = Hop s $ f <$> as
+
+instance Foldable (Labeled s) where
+    foldr f acc (Hop s as) = foldr f acc as
+
+instance Traversable (Labeled s) where
+    traverse f (Hop s as) = Hop s <$> traverse f as
