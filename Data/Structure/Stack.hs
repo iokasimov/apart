2ea97653799fc200ba2ca8e805025b2f4b01ef11
Murat Kasimov <iokasimov.m@gmail.com>
2018-04-29 15:16:18 +0300

    Add push for Stack and insert for Binary

--- Data/Structure/Stack.hs
+++ Data/Structure/Stack.hs
@@ -1,5 +1,8 @@
-module Data.Structure.Stack (Stack) where
+module Data.Structure.Stack (Stack, push) where
 
 import Control.Comonad.Cofree
 
 type Stack a = Cofree Maybe a
+
+push :: a -> Stack a -> Stack a
+push x = (:<) x . Just
--- Data/Structure/Tree/Binary.hs
+++ Data/Structure/Tree/Binary.hs
@@ -1,10 +1,10 @@
-module Data.Structure.Tree.Binary (Binary) where
+module Data.Structure.Tree.Binary (Binary, insert, example) where
 
 import Control.Comonad.Cofree
 
 type Binary a = Cofree Crotch a
 
-data Crotch a = End | Less a | Greater a | Crotch a a
+data Crotch a = End | Less a | Greater a | Crotch a a deriving Show
 
 instance Functor Crotch where
 	fmap f End = End
@@ -18,3 +18,14 @@ instance Foldable Crotch where
 	foldr f acc (Less l) = f l acc
 	foldr f acc (Greater r) = f r acc
 	foldr f acc (Crotch l r) = f l $ f r acc
+
+insert :: Ord a => Binary a -> a -> Binary a
+insert (y :< End) x@((>) y -> True) = y :< Less (x :< End)
+insert (y :< End) x@((<) y -> True) = y :< Greater (x :< End)
+insert (y :< Less lt) x@((>) y -> True) = y :< Less (insert lt x)
+insert (y :< Less lt) x@((<) y -> True) = y :< Crotch lt (x :< End)
+insert (y :< Greater gt) x@((>) y -> True) = y :< Crotch (x :< End) gt
+insert (y :< Greater gt) x@((<) y -> True) = y :< Greater (insert gt x)
+insert (y :< Crotch lt gt) x@((>) y -> True) = y :< Crotch (insert lt x) gt
+insert (y :< Crotch lt gt) x@((<) y -> True) = y :< Crotch lt (insert gt x)
+insert binary x = binary
