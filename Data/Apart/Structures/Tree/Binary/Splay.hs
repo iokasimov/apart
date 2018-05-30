9c33b89e3b4ddcc5fe187ffc3fb71e33cde295a6
Murat Kasimov <iokasimov.m@gmail.com>
2018-05-30 16:28:12 +0300

    Add splay function, remove parent key from predicates

--- Data/Apart/Structures/Tree/Binary/Splay.hs
+++ Data/Apart/Structures/Tree/Binary/Splay.hs
@@ -3,35 +3,42 @@ module Data.Apart.Structures.Tree.Binary.Splay (Splay) where
 import Control.Comonad (Comonad (..))
 import Data.Functor.Bind (Bind (..))
 import Data.Functor.Contravariant (Predicate (..))
+import Data.Function ((&))
 
+import Data.Apart.Apart (Segment (..))
 import Data.Apart.Structures.Tree.Binary (Binary, ls, gt)
+import Data.Apart.Structures.Tree.Binary.Rotation (Rotate (..), rtt)
 
 type Splay = Binary
 
-left_zig :: Eq a => Predicate (a, a, Binary a)
-left_zig = Predicate $ \ (x, p, t) -> extract t == p &&
-	(foldr (\g _ -> extract g == x) False $ gt t)
+left_zig :: Eq a => Predicate (a, Binary a)
+left_zig = Predicate $ \ (x, t) -> gt t
+	& foldr (\g _ -> extract g == x) False
 
-right_zig :: Eq a => Predicate (a, a, Binary a)
-right_zig = Predicate $ \ (x, p, t) -> extract t == p &&
-	(foldr (\l _ -> extract l == x) False $ ls t)
+right_zig :: Eq a => Predicate (a, Binary a)
+right_zig = Predicate $ \ (x, t) -> ls t
+	& foldr (\l _ -> extract l == x) False
 
-left_zig_zig :: Eq a => Predicate (a, a, Binary a)
-left_zig_zig = Predicate $ \ (x, p, t) ->
-	(foldr (\g _ -> extract g == p) False $ gt t) &&
-	(foldr (\gg _ -> extract gg == x) False $ gt t >>- gt)
+left_zig_zig :: Eq a => Predicate (a, Binary a)
+left_zig_zig = Predicate $ \ (x, t) -> gt t >>- gt
+	& foldr (\gg _ -> extract gg == x) False
 
-right_zig_zig :: Eq a => Predicate (a, a, Binary a)
-right_zig_zig = Predicate $ \ (x, p, t) ->
-	(foldr (\l _ -> extract l == p) False $ ls t) &&
-	(foldr (\ll _ -> extract ll == x) False $ ls t >>- ls)
+right_zig_zig :: Eq a => Predicate (a, Binary a)
+right_zig_zig = Predicate $ \ (x, t) -> ls t >>- ls
+	& foldr (\ll _ -> extract ll == x) False
 
-left_zig_zag :: Eq a => Predicate (a, a, Binary a)
-left_zig_zag = Predicate $ \ (x, p, t) ->
-	(foldr (\l _ -> extract l == p) False $ gt t) &&
-	(foldr (\ll _ -> extract ll == x) False $ gt t >>- ls)
+left_zig_zag :: Eq a => Predicate (a, Binary a)
+left_zig_zag = Predicate $ \ (x, t) -> gt t >>- ls
+	& foldr (\ll _ -> extract ll == x) False
 
-right_zig_zag :: Eq a => Predicate (a, a, Binary a)
-right_zig_zag = Predicate $ \ (x, p, t) ->
-	(foldr (\l _ -> extract l == p) False $ ls t) &&
-	(foldr (\ll _ -> extract ll == x) False $ ls t >>- gt)
+right_zig_zag :: Eq a => Predicate (a, Binary a)
+right_zig_zag = Predicate $ \ (x, t) -> ls t >>- gt
+	& foldr (\ll _ -> extract ll == x) False
+
+splay :: Eq a => a -> Binary a -> Segment Splay a
+splay x t@(getPredicate left_zig . (x,) -> True) = rtt L t
+splay x t@(getPredicate right_zig . (x,) -> True) = rtt R t
+splay x t@(getPredicate left_zig_zig . (x,) -> True) = rtt LL t
+splay x t@(getPredicate right_zig_zig . (x,) -> True) = rtt RR t
+splay x t@(getPredicate left_zig_zag . (x,) -> True) = rtt RL t
+splay x t@(getPredicate right_zig_zag . (x,) -> True) = rtt LR t
