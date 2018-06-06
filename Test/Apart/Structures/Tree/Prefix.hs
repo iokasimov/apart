32989b3da2202252aa29b55b9afdf3b126dc7364
Murat Kasimov <iokasimov.m@gmail.com>
2018-06-06 08:54:00 +0300

    [WIP] Tests on Prefix tree

--- Test/Apart/Structures/Tree/Prefix.hs
+++ Test/Apart/Structures/Tree/Prefix.hs
@@ -1,3 +1,6 @@
 module Test.Apart.Structures.Tree.Prefix where
 
 import Data.Apart.Structures.Tree.Prefix (Prefix, singleton)
+
+gen_singleton_prefix_tree :: Gen (Prefix Char Int [])
+gen_singleton_prefix_tree = singleton <$> enumBounded <$> enumBounded
