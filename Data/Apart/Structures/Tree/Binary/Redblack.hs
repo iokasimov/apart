d58183d2786ec21232b44e4d22a60209ba43b831
Murat Kasimov <iokasimov.m@gmail.com>
2018-06-11 15:28:10 +0300

    Add paint function: from binary to red-black

--- Data/Apart/Structures/Tree/Binary/Redblack.hs
+++ Data/Apart/Structures/Tree/Binary/Redblack.hs
@@ -1,6 +1,20 @@
-module Data.Apart.Structures.Tree.Binary.Redblack (Redblack, Color (..)) where
+module Data.Apart.Structures.Tree.Binary.Redblack (Redblack, Color (..), paint) where
 
-import Data.Apart.Structures.Tree.Binary (Binary)
+import Control.Comonad.Cofree (Cofree (..))
 
-data Color = Red | Black
+import Data.Apart.Structures.Tree.Binary (Binary, Branches (..))
+
+data Color = Red | Black deriving Show
+
 type Redblack a = Binary (a, Color)
+
+paint :: Binary a -> Redblack a
+paint tree = step Black tree where
+
+	step :: Color -> Binary a -> Redblack a
+	step color (x :< End) = (x, Black) :< End
+	step color (x :< rest) = (x, color) :< (step (invert color) <$> rest)
+
+	invert :: Color -> Color
+	invert Black = Red
+	invert Red = Black
