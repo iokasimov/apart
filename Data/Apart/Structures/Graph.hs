6848275eb952aba9ea42b0a2ef1ae133fc5d6d7d
Murat Kasimov <iokasimov.m@gmail.com>
2018-07-16 17:50:30 +0300

    Get rid Single constructor for Graph strucutre

--- Data/Apart/Structures/Graph.hs
+++ Data/Apart/Structures/Graph.hs
@@ -8,28 +8,24 @@ import Data.Apart.Apart (Segment (..))
 -- | Directed acyclic graph.
 type Graph = Cofree Edge
 
-data Edge a = Empty | Single a | Connect a | Overlay a deriving Show
+data Edge a = Empty | Connect a | Overlay a deriving Show
 
 instance Functor Edge where
 	fmap f Empty = Empty
-	fmap f (Single x) = Single $ f x
 	fmap f (Connect x) = Connect $ f x
 	fmap f (Overlay x) = Overlay $ f x
 
 instance Foldable Edge where
 	foldr f acc Empty = acc
-	foldr f acc (Single x) = f x acc
 	foldr f acc (Connect x) = f x acc
 	foldr f acc (Overlay x) = f x acc
 
 instance Traversable Edge where
 	traverse f Empty = pure Empty
-	traverse f (Single x) = Connect <$> f x
 	traverse f (Connect x) = Connect <$> f x
 	traverse f (Overlay x) = Overlay <$> f x
 
-single, connect, overlay, empty :: Segment Graph a -> Segment Graph a
-single = foldr (\x _ -> Single x) Empty
+connect, overlay, empty :: Segment Graph a -> Segment Graph a
 connect = foldr (\x _ -> Connect x) Empty
 overlay = foldr (\x _ -> Overlay x) Empty
 empty = const Empty
